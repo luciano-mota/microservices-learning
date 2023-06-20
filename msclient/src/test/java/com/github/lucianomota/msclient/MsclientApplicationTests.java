46efbc3d94f003282d7ee650102f028e17139e31
luciano-mota <amoosenhorjesus3@gmail.com>
2023-06-19 22:44:17 -0300

    Eureka Serve e serviço simples de cliente.

---
+++ msclient/src/test/java/com/github/lucianomota/msclient/MsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucianomota.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
